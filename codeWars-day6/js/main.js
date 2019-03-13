058fe6dfdc3e298e49fd527195163904f58d70c3
Kinda <Kinda@Kindas-MacBook-Air.local>
2019-03-12 23:24:15 -0400

    Find the smallest integer in the array| code Wars day 6

---
+++ codeWars-day6/js/main.js
@@ -0,0 +1,14 @@
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    if (args.length === 1) {
+      return args[0];
+    }
+    var smallestInt = args[0];
+    for (var i = 0; i < args.length; i++) {
+      if (smallestInt > args[i]) {
+        smallestInt = args[i];
+      }
+    }
+    return smallestInt;
+  }
+}
