9b89ce5494371f5e32c770dd08bafdb34931380a
Kinda <Kinda@Kindas-MacBook-Air.local>
2019-03-20 23:45:22 -0400

    Opposite Number JS

---
+++ codeWars-day12/README.md
@@ -0,0 +1,6 @@
+Opposite number
+
+Very simple, given a number, find its opposite.
+
+
+![alt](oppositeNumber.png)
---
+++ codeWars-day12/js/main.js
@@ -0,0 +1,9 @@
+//Opposite number
+
+
+//Very simple, given a number, find its opposite.
+function opposite(number) {
+
+  return (number<=0 ? number*-1: -number);
+
+}
