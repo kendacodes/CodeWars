3dd4cc496189af46ffe6ab12f3044c0d83b8c515
Kinda <Kinda@Kindas-MacBook-Air.local>
2019-03-21 22:14:44 -0400

    Reverse List 8kyu

---
+++ codeWars-day13/main.js
@@ -0,0 +1,8 @@
+//Reverse List Order
+
+//In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+function reverseList(list) {
+  let myarrayRev=list.reverse()
+return (myarrayRev)
+}
