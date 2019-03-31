c63dcdedc0f5c90c777ca25c7860eeed6e68d589
KendaCodes <K.Lamandier@gmail.com>
2019-03-31 13:01:18 -0400

    solutions Hello world

---
+++ codeWars-day14.1/main.js
@@ -0,0 +1,10 @@
+// 8 kyu
+
+
+// Function 1 - hello world
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Write a function "greet" that returns "hello world!"
+function greet(){
+ return "hello world!"
+}
---
+++ codeWars-day14.1/readme.md
@@ -0,0 +1,9 @@
+// 8 kyu
+
+
+// Function 1 - hello world
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Write a function "greet" that returns "hello world!"
+
+![alt](hello.png)
