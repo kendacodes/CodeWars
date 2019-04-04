df7f94373508cb2775fe020eb071be6f74c34799
KendaCodes <K.Lamandier@gmail.com>
2019-04-04 16:58:28 -0400

    Grasshopper - Debug sayHello

---
+++ codeWars-day21/main.js
@@ -0,0 +1,11 @@
+//8 kyu
+
+// Grasshopper - Debug sayHello
+
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+function sayHello(name){
+//concatenating the two words
+  return ('Hello, ' + name)
+}
