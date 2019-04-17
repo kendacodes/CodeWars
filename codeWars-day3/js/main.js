955629da76296ce4bfc5ce1d3a9c9b6bc9cba11b
Kenda <47069318+kendacodes@users.noreply.github.com>
2019-04-17 14:30:33 -0400

    Update main.js

--- codeWars-day3/js/main.js
+++ codeWars-day3/js/main.js
@@ -1,6 +1,7 @@
 //Create a function isDivisible(n, x, y) that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
 function isDivisible(n, x, y) {
 console.log(n)
+ //checking if n is divisible by both x and y
 if (n % x===0 && n % y=== 0){
 return true;
 }else{
